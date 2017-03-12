5c56227ee8a8bd089ec59f491b51f451ab6a04dc
Tymoteusz Adamczyk <tymoteusz.adamczyk@outlook.com>
2017-03-12 14:19:36 +0100

    Added members count to view

--- AprilisJam/Controllers/RegistrationController.cs
+++ AprilisJam/Controllers/RegistrationController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using AprilisJam.Data;
 using AprilisJam.Models;
 using AprilisJam.Services;
@@ -12,15 +13,23 @@ namespace AprilisJam.Controllers
     {
         private IConfirmationEmailSender _confirmationEmailSender { get; }
         private AprilisJamRegistrationContext _context { get; }
+        private EmailContent _emailContent { get; }
 
-        public RegistrationController(IConfirmationEmailSender confirmationEmailSender, AprilisJamRegistrationContext context)
+        public RegistrationController(IConfirmationEmailSender confirmationEmailSender, AprilisJamRegistrationContext context, IOptions<EmailContent> emailContent)
         {
             _confirmationEmailSender = confirmationEmailSender;
             _context = context;
+            _emailContent = emailContent.Value;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            int memberCount = await _context.RegistrationForms.CountAsync();
+            if (memberCount >= _emailContent.MemberThreshold)
+                ViewData["MemberCountMessage"] = $"Niestety, wszystkie miejsca s¹ ju¿ zajête :( <br />Mo¿esz wpisaæ siê na listê rezerwow¹, bêdziesz {memberCount + 1 - _emailContent.MemberThreshold} w kolejce.";
+            else
+                ViewData["MemberCountMessage"] = $"Iloœæ wolnych miejsc: {_emailContent.MemberThreshold - memberCount}";
+
             return View();
         }
 
